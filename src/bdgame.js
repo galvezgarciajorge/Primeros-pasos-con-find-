a3dc69288e0bea76decaf252c048951ba8650be5
github_galvezgarciajorge <jgalvez@iespuntadelverde.es>
2020-10-30 00:43:00 +0100

    first commit

---
+++ src/bdgame.js
@@ -0,0 +1,77 @@
+db.bdgame.insertMany([
+        {
+            nombre: "Counter-Strike", 
+            recaudacion: ["258.000.000"],
+            compañia: "VALVE Software",
+            tipo: ["Disparos", "Accion"],
+            estreno: 19/06/1999, 
+            dificultad: "****"
+        },
+        {           
+            nombre: "Overwatch", 
+            recaudacion: ["585.600.000"],
+            compañia: "Blizzard",
+            tipo: ["Disparos", "Accion"],
+            estreno: 24/05/2016,
+            dificultad: "**",
+        },
+        {
+            nombre: "Minecraft",
+            recaudacion: ["88.700.000"],
+            compañia: "Mojang AB",
+            tipo: ["Sandbox","Construccion"],
+            estreno: 18/11/2009, 
+            dificultad: "*",
+        },
+        {
+            nombre: "Warcraft", 
+            recaudacion: ["4.000.000"],
+            compañia: "Blizzard", 
+            tipo: ["Batalla", "Estrategia"],
+            estreno: 05/04/2004,
+            dificultad: "****",
+        },
+        {
+            nombre: "League of Legends",
+            recaudacion: ["21.500.000"],
+            compañia: "Riot Games", 
+            tipo: ["Batalla", "Estrategia"],
+            estreno: 29/10/2009,
+            dificultad: "****",
+        },
+        {
+            nombre: "Age of Empires",
+            recaudacion: ["215.000.000"],
+            compañia: "Ensemble Studios", 
+            tipo: ["Estrategia", "Construccion"],
+            estreno: 26/10/1997, 
+            dificultad: "***",
+        },
+        {
+            nombre: "Fornite", 
+            recaudacion: ["5.000.000.000"],
+            compañia: "Epic game", 
+            tipo: ["Disparos","Accion"],
+            estreno:  21/06/2017, 
+            dificultad: "*****",
+        },
+        {
+            nombre: "Roblox", 
+            recaudacion: ["120.000.000"],
+            compañia: "Roblox Corporation", 
+            tipo: ["Sandbox", "Construccion"],
+            estreno: 27/02/2006, 
+            dificultad: "**",
+        },
+        {
+            nombre: "GTA V", 
+            recaudacion: ["500.000.000"],
+            compañia: "Rockstar Games",
+            tipo: ["Accion", "Aventura"],
+            estreno: 17/09/2013, 
+            dificultad: "***",
+        }]);
+
+
+
+
